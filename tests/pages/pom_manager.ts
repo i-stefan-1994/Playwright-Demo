ceff13f2f6fc6d0c1c89f5419194e19cb02dc73e
Stefan <60820561+i-stefan-1994@users.noreply.github.com>
2025-04-06 11:54:14 +0300

    Update pom_manager.ts

--- tests/pages/pom_manager.ts
+++ tests/pages/pom_manager.ts
@@ -2,26 +2,23 @@ import { Page } from "@playwright/test";
 import { DemoQA_HomePage } from "./demoqa_home_page";
 import { ElementsPage } from "./elements_page";
 
-
-export class POM_Manager{
-
+export class POM_Manager {
     private demoQA_homePage: DemoQA_HomePage | undefined;
     private demoQA_elementsPage: ElementsPage | undefined;
 
-    constructor(private page: Page){
-        
-    }
+    constructor(private page: Page) {}
 
-    getDemoQA_HomePage(){
-        if(!this.demoQA_homePage){
+    getDemoQA_HomePage() {
+        if (!this.demoQA_homePage) {
             this.demoQA_homePage = new DemoQA_HomePage(this.page);
         }
         return this.demoQA_homePage;
     }
-  
-    getDemoQA_ElementsPage(){
-        if(!this.demoQA_elementsPage){
+
+    getDemoQA_ElementsPage() {
+        if (!this.demoQA_elementsPage) {
             this.demoQA_elementsPage = new ElementsPage(this.page);
         }
         return this.demoQA_elementsPage;
     }
+}
